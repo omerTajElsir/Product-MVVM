c0875c01e695794caaa56cde5c7c2b06a9e26477
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-07-15 16:23:33 -0500

    Migrate package name

---
+++ app/src/test/java/io/github/erikjhordanrey/people_mvvm/MockView.java
@@ -0,0 +1,11 @@
+package io.github.erikjhordanrey.people_mvvm;
+
+import android.content.Context;
+import android.view.View;
+
+public class MockView extends View {
+
+    public MockView(Context context) {
+        super(context);
+    }
+}
